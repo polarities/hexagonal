90aa1f225594f83e1f5410d6f075ea4e03c26e23
Tuna Lee <10000685+polarities@users.noreply.github.com>
2022-06-22 20:11:37 +0200

    Initial Commit.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='HexImage',
+    version='0.0.0',
+    packages=['haxagonal', 'haxagonal.parse', 'haxagonal.visualization'],
+    url='',
+    license='',
+    author='Lee',
+    author_email='',
+    description='Hexagonal pixel image handling library.'
+)
